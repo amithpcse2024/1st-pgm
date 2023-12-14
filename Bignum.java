73b7b5b7ec98f9f6d1b4e98d8f008e581d20c292
Amith P <143409183+amithpcse2024@users.noreply.github.com>
2023-12-14 14:43:15 +0530

    Add files via upload

---
+++ Bignum.java
@@ -0,0 +1,21 @@
+package big;
+
+public class Bignum {
+
+	public static void main(String[] args) {
+	int	x=10,y=20,z=30;
+	if (x>y && x>z) {
+		System.out.println(x+" is greater");
+		
+		}
+	else if(y>x && y>z) {
+		System.out.println(y+" is greater");
+	}
+	else {
+		System.out.println(z+" is greater");
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+}
